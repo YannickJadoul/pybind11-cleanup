d9afc74ec1f3a0a87a48286964013d3fd2214c87
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-05-14 14:53:25 +0300

    fix: add missing ) in sed script

--- replace_issue_links.sh
+++ replace_issue_links.sh
@@ -1,1 +1,1 @@
-sed -i -E 's/(^|[^[])(pybind\/pybind11)#([0-9]+)/\1[\2#\3](https:\/\/github.com\/\2\/issues\/\3/g' *.md
+sed -i -E 's/(^|[^[])(pybind\/pybind11)#([0-9]+)/\1[\2#\3](https:\/\/github.com\/\2\/issues\/\3)/g' *.md
